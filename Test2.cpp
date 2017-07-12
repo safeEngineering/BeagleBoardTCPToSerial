5065b07f067204543516a3be6f88a1f17969c723
Adrian Edmonds <adrian@safe-engineering.com.au>
2017-07-12 15:00:58 +1000

    Hello World First Commit

---
+++ Test2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Test2.cpp
+// Author      : AE
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World  ARM!!!" << endl; // prints !!!Hello World ARM!!!
+	return 0;
+}
