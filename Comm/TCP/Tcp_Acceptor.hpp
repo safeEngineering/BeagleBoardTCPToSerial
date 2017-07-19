8bc477e1ae6741ecfe7677f763c6d2a3d2a88e7a
Phuoc Dai <nguyen.phuoc.ddai@gmail.com>
2017-07-19 13:57:16 +0700

    + Fix error when master was started before submaster is running + Change submaster's behavious that only accepts one connection on each endpoint

--- Comm/TCP/Tcp_Acceptor.hpp
+++ Comm/TCP/Tcp_Acceptor.hpp
@@ -19,15 +19,17 @@ namespace SafeEngineering
             Acceptor(asio::io_service& io_service, Serial& serial, const std::string& host, int port) : m_ios(io_service), m_serial(serial),
                 m_acceptor(io_service, asio::ip::tcp::endpoint(asio::ip::address::from_string(host), port))
             {
-                
+                m_haveConnection = false;
             }
             
             bool AcceptConnections()
             {
                 try
-                {
+                {   
                     SafeEngineering::Comm::Connection::pointer new_connection = boost::shared_ptr<Connection>(new Connection(m_ios, m_serial));
-                    
+                    // Register Dropped event
+                    new_connection->m_ConnectionDropped.connect(boost::bind(&Acceptor::HandleDroppedConnection, this));
+   
                     std::cout << "Waiting for new client" << std::endl;
                     m_acceptor.async_accept(new_connection->Socket(),
                         boost::bind(&Acceptor::HandleAccept, this, new_connection, asio::placeholders::error));
@@ -45,9 +47,26 @@ namespace SafeEngineering
             {
                 if(!err)
                 {
-                    std::cout << "Accepted request from endpoint:" << new_connection->Socket().remote_endpoint() << std::endl;
-                    // Initialize data reading for new connection
-                    new_connection->Start();
+                    /////////////////////////////////////////////////////////////////////////////////////////////////////
+                    // Now, we no need to guard m_haveConnection variable because all opeartions were in the same thread
+                    // However, the synchronous was needed whenever we call asio::io_service::run from more than threads
+                    /////////////////////////////////////////////////////////////////////////////////////////////////////
+                    if(m_haveConnection == false)
+                    {
+                        // Toggle the flag to diable new connection
+                        m_haveConnection = true;
+                        
+                        std::cout << "Accepted request from endpoint:" << new_connection->Socket().remote_endpoint() << std::endl;
+                        // Register Data event from Serial class
+                        new_connection->RegisterSerialData();
+                        // Initialize data reading for new connection
+                        new_connection->Start();
+                    }
+                    else
+                    {
+                        std::cout << "Cancel request from endpoint:" << new_connection->Socket().remote_endpoint() << std::endl;
+                        new_connection->Socket().close();
+                    }
                     
                     // Listen to new request
                     if(!AcceptConnections())
@@ -58,7 +77,16 @@ namespace SafeEngineering
                 else
                 {
                     std::cerr << "Failed: '" << err.message() <<"' when accepting new connection" << std::endl;
-                }
+                }                
+            }
+            
+            void HandleDroppedConnection()
+            {
+                /////////////////////////////////////////////////////////////////////////////////////////////////////
+                // Now, we no need to guard m_haveConnection variable because all opeartions were in the same thread
+                // However, the synchronous was needed whenever we call asio::io_service::run from more than threads
+                /////////////////////////////////////////////////////////////////////////////////////////////////////
+                m_haveConnection = false;   // Toggle the flag to enable new connection
             }
             
         private:
@@ -67,7 +95,9 @@ namespace SafeEngineering
             // UART/COM serial object
             Serial& m_serial;
             // TCP/IP connection acceptance object
-            asio::ip::tcp::acceptor m_acceptor;            
+            asio::ip::tcp::acceptor m_acceptor;
+            // The flag specifies whethere having connection or not
+            bool m_haveConnection;
             
         };  // Acceptor class
         
--- Comm/TCP/Tcp_Connection.hpp
+++ Comm/TCP/Tcp_Connection.hpp
@@ -4,6 +4,8 @@
 #include <boost/bind.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/signals2.hpp>
 #include <asio.hpp>
 
 #include <Serial.hpp>
@@ -18,25 +20,37 @@ namespace SafeEngineering
         {   
         public:
             typedef boost::shared_ptr<Connection> pointer;
-            
-            //Connection(asio::io_service& io_service) : m_socket(io_service), m_COM(io_service)
+                        
             Connection(asio::io_service& io_service, Serial& serial) : m_socket(io_service), m_serial(serial)
             {
-            
+                // The default is server connection
+                m_serverSide = true;
             }
             
+            ~Connection()
+            {
+                std::cout << "Destroy TCP/IP connection" << std::endl;
+            }
+
+            // The event will signal whenever connection was dropped
+            boost::signals2::signal<void()> m_ConnectionDropped;
+
             // Get underlying TCP/IP socket
             asio::ip::tcp::socket& Socket()
             {
                 return m_socket;
             }
-                        
+            
+            // Register data from UART port
+            void RegisterSerialData()
+            {
+                // Register signal from serial object
+                m_serial.m_DataReceived.connect(boost::bind(&Connection::HandleSerialData, shared_from_this(), _1, _2));
+            }
+            
             // Start reading data from remote client
             void Start()
             {                
-                // First, register signal from serial object
-                m_serial.m_DataReceived.connect(boost::bind(&Connection::HandleSerialData, shared_from_this(), _1, _2));
-                
                 // Keep remote endpoint for logging
                 m_remoteEndpoint = m_socket.remote_endpoint();
                 
@@ -50,7 +64,17 @@ namespace SafeEngineering
             
             // Connect to remote server
             void Connect(const std::string& serverIP, int serverPort)
-            {                
+            { 
+                // Mark this connection as client connection
+                m_serverSide = false;
+                
+                // Save connection parameters for later use
+                m_serverIP = serverIP;
+                m_serverPort = serverPort;
+                
+                // Register serial Data
+                RegisterSerialData();
+                    
                 asio::ip::tcp::endpoint ep(asio::ip::address::from_string(serverIP), serverPort);
                 std::cout << "Trying to connect to " << ep << std::endl;
                 m_socket.async_connect(ep, boost::bind(&Connection::HandleConnect, shared_from_this(), asio::placeholders::error));
@@ -59,11 +83,24 @@ namespace SafeEngineering
             // Close the TCP/IP connection
             void Close()
             {
-                m_socket.close();
+                if(m_socket.is_open())
+                    m_socket.close();
             }
             
         private:
-            void HandleRead(const asio::error_code& err, std::size_t bytes_received)
+            // Reconnect to the remote server
+            void Reconnect()
+            {
+                // Delay 1s before reconnecing to the server
+                asio::deadline_timer delay(m_socket.get_io_service(), boost::posix_time::seconds(1));
+                delay.wait();
+                
+                asio::ip::tcp::endpoint ep(asio::ip::address::from_string(m_serverIP), m_serverPort);
+                std::cout << "Trying to reconnect to " << ep << std::endl;
+                m_socket.async_connect(ep, boost::bind(&Connection::HandleConnect, shared_from_this(), asio::placeholders::error));
+            }
+            
+            void HandleRead(const asio::error_code& err, const std::size_t& bytes_received)
             {
                 if(!err)
                 {
@@ -82,12 +119,22 @@ namespace SafeEngineering
                 }
                 else
                 {
-                    std::cout << "HandleRead closes the connection with endpoint:" << m_remoteEndpoint << std::endl;
+                    std::cerr << "Failed: '" << err.message() <<"' in HandleRead on endpoint:" << m_remoteEndpoint << std::endl;
                     m_socket.close();
+                    if(m_serverSide == true)
+                    {
+                        // Signal to acceptor that the connection was dropped
+                        m_ConnectionDropped();
+                    }
+                    else
+                    {
+                        // Try to reconnect to server
+                        Reconnect();
+                    }
                 }
             }
             
-            void HandleWrite(const asio::error_code& err, std::size_t bytes_sent)
+            void HandleWrite(const asio::error_code& err, const std::size_t& bytes_sent)
             {
                 if(!err)
                 {
@@ -95,8 +142,18 @@ namespace SafeEngineering
                 }
                 else
                 {
-                    std::cout << "HandleWrite closes the connection with endpoint:" << m_remoteEndpoint << std::endl;
+                    std::cerr << "Failed: '" << err.message() <<"' in HandleWrite on endpoint:" << m_remoteEndpoint << std::endl;
                     m_socket.close();
+                    if(m_serverSide == true)
+                    {
+                        // Signal to acceptor that the connection was dropped
+                        m_ConnectionDropped();
+                    }
+                    else
+                    {
+                        // Try to reconnect to server
+                        Reconnect();
+                    }
                 }
             }
             
@@ -109,12 +166,20 @@ namespace SafeEngineering
                 }
                 else
                 {
-                    std::cerr << "Failed: '" << err.message() <<"' when connecting to server" << std::endl;
+                    std::cerr << "Failed: '" << err.message() <<"' in HandleConnect" << std::endl;
+                    // Try to reconnect to server
+                    Reconnect();
                 }
             }
             
             void HandleSerialData(uint8_t* pPacket, uint8_t len)
             {
+                if(m_socket.is_open() == false)
+                {
+                    std::cerr << "TCP/IP connection have not established yet" << std::endl;
+                    return;
+                }
+                    
                 // Copy data from serial's buffer to local tx buffer
                 memcpy(m_txbuffer, pPacket, len);
                 m_txbufferLen = len;
@@ -139,6 +204,11 @@ namespace SafeEngineering
             
             // Remote endpoint of the connection
             asio::ip::tcp::endpoint m_remoteEndpoint;
+            // Server connection parameters (it used to reconnect to server)
+            std::string m_serverIP;
+            int m_serverPort;
+            // The flag specifies this connection is client side at or server side
+            bool m_serverSide;
             
         };  // Connection class
     }   
