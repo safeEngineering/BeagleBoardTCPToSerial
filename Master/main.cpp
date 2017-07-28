df77d44c07e82efe465aedd3e2a446185d5f0dad
Phuoc Dai <nguyen.phuoc.ddai@gmail.com>
2017-07-28 23:02:20 +0700

    Update code in main.cpp files for Master, Submaster and insert settings.json files in Step 1

--- Master/main.cpp
+++ Master/main.cpp
@@ -13,18 +13,53 @@ int main(int argc, char **argv)
 {
     try
     {
+        SafeEngineering::Utils::Settings appSettings;
+        if(SafeEngineering::Utils::LoadSettings(appSettings) == false)
+        {
+            std::cerr << "Main() function failed to load settings from json/text file" <<std::endl;
+            return -1;            
+        }
+        
+        // Double-check the ID value was declared for master
+        if(appSettings.CurrentUnit.ID != 0)
+        {
+            std::cerr << "Invalid ID in settings.json file" <<std::endl;
+            return -1;
+        }
+        
         asio::io_service ios;
-                
+        
+        // Construct serial object
         SafeEngineering::Comm::Serial serial1(ios);
         // Open UART connection
         serial1.OpenSerial();
-        SafeEngineering::Comm::Connection::pointer new_connection = boost::shared_ptr<SafeEngineering::Comm::Connection>(new SafeEngineering::Comm::Connection(ios, serial1));
-        //new_connection->Connect("192.168.1.101", 8000);
-        new_connection->Connect("10.10.1.107", 8000);
+        
+        // First, make a connection to submaster
+        for(int i = 0; i < (int)appSettings.Units.size(); i++)
+        {
+            if(appSettings.Units[i].Type == SafeEngineering::Utils::UnitType::SUBMASTER)
+            {
+                // Construct TCP/IP object
+                SafeEngineering::Comm::Connection::pointer new_connection = boost::shared_ptr<SafeEngineering::Comm::Connection>(new SafeEngineering::Comm::Connection(ios, serial1));
+                new_connection->Connect(appSettings.Units[i].IPAddress, 10001);
+            }
+        }
+                
+        // Second, make connections to slaves
+        for(int i = 0; i < (int)appSettings.Units.size(); i++)
+        {
+            if(appSettings.Units[i].Type == SafeEngineering::Utils::UnitType::SLAVE)
+            {
+                // Construct TCP/IP object
+                SafeEngineering::Comm::Connection::pointer new_connection = boost::shared_ptr<SafeEngineering::Comm::Connection>(new SafeEngineering::Comm::Connection(ios, serial1));
+                new_connection->Connect(appSettings.Units[i].IPAddress, 10001);
+                break;  // Now, we connect to one-only-one slave module
+            }
+        }
         
         // Run the ASIO service
         ios.run();
-        
+                    
         // Close UART connection
         serial1.CloseSerial();
     }
--- SubMaster/main.cpp
+++ SubMaster/main.cpp
@@ -13,16 +13,42 @@ int main(int argc, char **argv)
 {
     try
     {
+        // Load settings from json/text file
+        SafeEngineering::Utils::Settings appSettings;
+        if(SafeEngineering::Utils::LoadSettings(appSettings) == false)
+        {
+            std::cerr << "Main() function failed to load settings from json/text file" <<std::endl;
+            return -1;
+        }
+        
+        // Double-check the ID value was declared for submaster
+        if(appSettings.CurrentUnit.ID != 9)
+        {
+            std::cerr << "Invalid ID in settings.json file" <<std::endl;
+            return -1;
+        }
+        
         asio::io_service ios;
         
         SafeEngineering::Comm::Serial serial1(ios);
         // Open UART connection
         serial1.OpenSerial();
         
-        // Listen to new connections
-        //SafeEngineering::Comm::Acceptor acceptor(ios, serial1, "192.168.1.103", 8000);
-        SafeEngineering::Comm::Acceptor acceptor(ios, serial1, "10.10.1.144", 8000);
+        // First, listen to connections from master
+        SafeEngineering::Comm::Acceptor acceptor(ios, serial1, appSettings.CurrentUnit.IPAddress, 10001);
         acceptor.AcceptConnections();
+        
+        // Second, make connections to slaves
+        for(int i = 0; i < (int)appSettings.Units.size(); i++)
+        {
+            if(appSettings.Units[i].Type == SafeEngineering::Utils::UnitType::SLAVE)
+            {
+                // Construct TCP/IP object
+                SafeEngineering::Comm::Connection::pointer new_connection = boost::shared_ptr<SafeEngineering::Comm::Connection>(new SafeEngineering::Comm::Connection(ios, serial1));
+                new_connection->Connect(appSettings.Units[i].IPAddress, 10002);                
+                break;  // Now, we connect to one-only-one slave module
+            }
+        }
         
         // Run the ASIO service
         ios.run();
