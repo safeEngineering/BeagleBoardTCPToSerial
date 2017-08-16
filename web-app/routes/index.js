01ba0139cb5124871d8e7476d2a1e387ddaafb5d
Phuoc Dai <nguyen.phuoc.ddai@gmail.com>
2017-08-16 21:31:41 +0700

    Update the short guide file and remote unused view file (index3.ejs)

--- web-app/routes/index.js
+++ web-app/routes/index.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/general", function(req, res, next) {
+/* Add route for general page */
+router.get('/general', function(req, res, next) {
   res.render('index1', { title: 'Express' });
 });
 
-router.get("/network", function(req, res, next) {
+/* Add route for network settings page */
+router.get('/network', function(req, res, next) {
   res.render('index2', { title: 'Express' });
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index1', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
